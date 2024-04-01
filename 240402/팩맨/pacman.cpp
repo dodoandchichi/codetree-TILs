ec9ecf350ac9091655d6c6592953c40cfd965b59
dodoandchichi <100553243+dodoandchichi@users.noreply.github.com>
2024-04-02 02:27:05 +0900

    Title: 팩맨, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240402/팩맨/pacman.cpp
+++ 240402/팩맨/pacman.cpp
@@ -189,6 +189,11 @@ int main() {
         Simulate();
     }
     int ans = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans += mon[i][j].size();
+        }
+    }
     
     cout << ans;
     return 0;
