5b06f76d3969fe4b331ac928509c03dce62bf839
dodoandchichi <100553243+dodoandchichi@users.noreply.github.com>
2024-04-05 18:57:33 +0900

    Title: 싸움땅, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/싸움땅/battle-ground.cpp
+++ 240405/싸움땅/battle-ground.cpp
@@ -193,6 +193,8 @@ void Simulate() {
 
 int main() {
     // 입력:
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n >> m >> k;
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++) {
