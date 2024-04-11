39100a93fee6f71fffdcb7f4f3a2e74e4412080f
dodoandchichi <100553243+dodoandchichi@users.noreply.github.com>
2024-04-11 22:00:33 +0900

    Title: 격자 숫자 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240411/격자 숫자 놀이/matrix-number-play.cpp
+++ 240411/격자 숫자 놀이/matrix-number-play.cpp
@@ -1,80 +1,79 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <tuple>
+#include <algorithm>
 
-#define MAX_NUM 100
 #define MAX_N 100
+#define MAX_NUM 100
 using namespace std;
 
-int n = 3, m = 3;
-int r, c, k;
-int grid[MAX_N + 1][MAX_N + 1];
+int n = 3, m = 3, r, c, k;
+int grid[MAX_N+1][MAX_N+1];
 
 int Rowplay(int row){
-    vector<pair<int, int>> nums;
+    vector<pair<int, int>> nums; // 빈도수, 숫자
 
-    for(int i=1; i<=MAX_NUM; i++){
+    for(int num=1; num<=MAX_NUM; num++){
         int freq = 0;
         for(int col=0; col<m; col++){
-            if(grid[row][col] == i){
+            if(grid[row][col] == num)
                 freq++;
-            }
         }
-        if(freq) nums.push_back(make_pair(freq, i));
+        if(freq) nums.push_back(make_pair(freq, num));
     }
 
-    for(int col=0; col<m; col++){
-        grid[row][col] = 0;
-    }
+    for(int i=0; i<m; i++) grid[row][i] = 0;
 
     sort(nums.begin(), nums.end());
-    for(int col=0; col<nums.size(); col++){
+
+    for(int i=0; i<nums.size(); i++){
+        pair<int, int> temp = nums[i];
         int freq, num;
-        tie(freq, num) = nums[col];
-        grid[row][col*2] = num;
-        grid[row][col*2+1] = freq;
+        tie(freq, num) = temp;
+        grid[row][i*2] = num;
+        grid[row][i*2 + 1] = freq;
     }
-    return (int) 2*nums.size();
+
+    return (int) nums.size() * 2;
 }
 
 int Colplay(int col){
-    vector<pair<int, int>> nums;
+    vector<pair<int, int>> nums; // 빈도수, 숫자
 
-    for(int i=1; i<=MAX_NUM; i++){
+    for(int num=1; num<=MAX_NUM; num++){
         int freq = 0;
         for(int row=0; row<n; row++){
-            if(grid[row][col] == i){
+            if(grid[row][col] == num)
                 freq++;
-            }
         }
-        if(freq) nums.push_back(make_pair(freq, i));
+        if(freq) nums.push_back(make_pair(freq, num));
     }
 
-    for(int row=0; row<n; row++){
-        grid[row][col] = 0;
-    }
+    for(int i=0; i<n; i++) grid[i][col] = 0;
 
     sort(nums.begin(), nums.end());
-    for(int row=0; row<nums.size(); row++){
+
+    for(int i=0; i<nums.size(); i++){
+        pair<int, int> temp = nums[i];
         int freq, num;
-        tie(freq, num) = nums[row];
-        grid[row*2][col] = num;
-        grid[row*2+1][col] = freq;
+        tie(freq, num) = temp;
+        grid[i*2][col] = num;
+        grid[i*2+1][col] = freq;
     }
-    return (int) 2*nums.size();
+
+    return (int) nums.size() * 2;
 }
 
 void Simulate(){
-    if(n >= m){
-        int maxCol = 0;
+    int maxCol = 0, maxRow = 0;
+
+    if(n>=m){
         for(int i=0; i<n; i++){
             maxCol = max(maxCol, Rowplay(i));
         }
         m = maxCol;
     }
     else{
-        int maxRow = 0;
         for(int i=0; i<m; i++){
             maxRow = max(maxRow, Colplay(i));
         }
@@ -86,24 +85,20 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     cin >> r >> c >> k;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
             cin >> grid[i][j];
         }
     }
 
     int ans = -1;
-    for(int i=0; i<=100; i++){
-        if(grid[r-1][c-1] == k) {
+    for(int i=1; i<=100; i++){
+        Simulate();
+        if(grid[r-1][c-1] == k){
             ans = i;
             break;
         }
-        Simulate();
     }
-    cout << ans;
+    cout << ans-1;
     return 0;
 }
-
-/*
-    어려워보여서 해설봤는데 해설보니까 이해가 잘됨
-*/
